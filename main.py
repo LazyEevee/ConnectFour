43f62853168e1dc362b2208ba4abb8b66d777d69
Christine Dimmer <chrissy.1989@yahoo.co.uk>
2022-06-07 15:00:27 +0100

    added/amended comments

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ FPS = 60
 
 BLACK = (0, 0, 0)  # background
 BLUE = (0, 0, 255)  # board
-RED = (255, 0, 0)  # player 1 pieces
-YELLOW = (255, 255, 0)  # player 2 pieces
+YELLOW = (255, 255, 0)  # player 1 pieces
+RED = (255, 0, 0)  # player 2 pieces
 
 RADIUS = 40  # radius of player pieces and holes in board
 
@@ -26,9 +26,11 @@ COLUMNS = {
     "column_6": [],
     "column_7": [],
 }
+ROWS = [550, 450, 350, 250, 150, 50]
 
 
 def display_turns(player_one):
+    """ blit to screen whose turn it is """
     if player_one:
         WINDOW.blit(TEXT.render("Player 1", True, YELLOW), (20, 620))
     else:
@@ -36,6 +38,7 @@ def display_turns(player_one):
 
 
 def draw(window):
+    """ draw the game grid on screen """
     window.fill(BLUE)
     for x in range(50, WIDTH, 100):
         for y in range(50, HEIGHT-100, 100):
@@ -44,30 +47,33 @@ def draw(window):
 
 
 def place_yellow_counter(mouse_x, mouse_y, column):
+    """ add YELLOW counters to COLUMNS lists """
     if mouse_x > 0 and mouse_y < 600 and len(COLUMNS["column_{0}".format(column)]) < 7:
         COLUMNS["column_{0}".format(column)].append(YELLOW)
 
 
 def place_red_counter(mouse_x, mouse_y, column):
+    """ add RED counters to COLUMNS lists """
     if mouse_x > 0 and mouse_y < 600 and len(COLUMNS["column_{0}".format(column)]) < 7:
         COLUMNS["column_{0}".format(column)].append(RED)
 
 
-def draw_counter(rows):
+def draw_counter():
+    """ draw counters into grid """
     for i in range(len(COLUMNS["column_1"])):
-        pygame.draw.circle(WINDOW, COLUMNS["column_1"][i], [50, rows[i]], RADIUS, 0)
+        pygame.draw.circle(WINDOW, COLUMNS["column_1"][i], [50, ROWS[i]], RADIUS, 0)
     for i in range(len(COLUMNS["column_2"])):
-        pygame.draw.circle(WINDOW, COLUMNS["column_2"][i], [150, rows[i]], RADIUS, 0)
+        pygame.draw.circle(WINDOW, COLUMNS["column_2"][i], [150, ROWS[i]], RADIUS, 0)
     for i in range(len(COLUMNS["column_3"])):
-        pygame.draw.circle(WINDOW, COLUMNS["column_3"][i], [250, rows[i]], RADIUS, 0)
+        pygame.draw.circle(WINDOW, COLUMNS["column_3"][i], [250, ROWS[i]], RADIUS, 0)
     for i in range(len(COLUMNS["column_4"])):
-        pygame.draw.circle(WINDOW, COLUMNS["column_4"][i], [350, rows[i]], RADIUS, 0)
+        pygame.draw.circle(WINDOW, COLUMNS["column_4"][i], [350, ROWS[i]], RADIUS, 0)
     for i in range(len(COLUMNS["column_5"])):
-        pygame.draw.circle(WINDOW, COLUMNS["column_5"][i], [450, rows[i]], RADIUS, 0)
+        pygame.draw.circle(WINDOW, COLUMNS["column_5"][i], [450, ROWS[i]], RADIUS, 0)
     for i in range(len(COLUMNS["column_6"])):
-        pygame.draw.circle(WINDOW, COLUMNS["column_6"][i], [550, rows[i]], RADIUS, 0)
+        pygame.draw.circle(WINDOW, COLUMNS["column_6"][i], [550, ROWS[i]], RADIUS, 0)
     for i in range(len(COLUMNS["column_7"])):
-        pygame.draw.circle(WINDOW, COLUMNS["column_7"][i], [650, rows[i]], RADIUS, 0)
+        pygame.draw.circle(WINDOW, COLUMNS["column_7"][i], [650, ROWS[i]], RADIUS, 0)
 
 
 def main():
@@ -83,8 +89,7 @@ def main():
         mouse_x = position[0]
         mouse_y = position[1]
         column = (int(ceil(mouse_x / 100)))
-        rows = [550, 450, 350, 250, 150, 50]
-        draw_counter(rows)
+        draw_counter()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
